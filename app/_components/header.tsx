4e246e2dddbcd8ffcdde143748b148991051b782
adventuresEnglish <149850689+adventuresEnglish@users.noreply.github.com>
2024-06-04 19:39:39 -0500

    fix

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,15 +1,13 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Logo from "./Logo";
+import Navigation from "./Navigation";
 
-function Header() {
+export default function Header() {
   return (
     <header className="border-b border-primary-900 px-8 py-5">
-      <div className="flex justify-between items-center max-w-7xl mx-auto">
+      <div className="mx-auto flex max-w-7xl items-center justify-between">
         <Logo />
         <Navigation />
       </div>
     </header>
   );
 }
-
-export default Header;
